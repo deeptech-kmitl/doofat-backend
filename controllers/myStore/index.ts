d2ee078b90c3d50ab9c696e4bab92bbc59cfee3d
Ton <88500701+64070189Plankton@users.noreply.github.com>
2024-02-19 20:28:27 +0700

    add index

---
+++ controllers/myStore/index.ts
@@ -0,0 +1,8 @@
+export { default as create } from './create.controller'
+export { default as edit } from './edit.controller'
+export { default as getStores } from './getStores.controller'
+export { default as getStore } from './getStore.controller'
+export { default as deleteStore } from './delete.controller'
+
+export { default as menu } from './menu/index'
+export { default as food } from './food/index'
