1909c5ffaf707bf3e3c55384036a787b44c2fbbd
Ton <88500701+64070189Plankton@users.noreply.github.com>
2024-03-06 20:44:12 +0700

    add index food

---
+++ controllers/myStore/food/index.ts
@@ -0,0 +1,15 @@
+import getFoods from './getFoods.controller'
+import getFood from './getFood.controller'
+import createFood from './createFood.controller'
+import editFood from './editFood.controller'
+import deleteFood from './deleteFood.controller'
+import getStoreFoods from './getStoreFoods.controller'
+
+export default {
+    getFoods,
+    getFood,
+    createFood,
+    editFood,
+    deleteFood,
+    getStoreFoods
+}
